0ebea33a915832e32b62447470f3f5de9ecc201b
diego <diegotaveira1996@gmail.com>
2022-10-07 09:02:54 -0300

    simulador de investimento

---
+++ juros.js
@@ -0,0 +1,22 @@
+
+
+// funcao pra calcular o investimento-----------------
+
+
+// adinionar a classe active quando clicar no botao-------------
+
+
+
+// limpar os inputs-------------------------------------------
+
+
+// modal-------------------------------------------------------
+
+
+
+
+
+
+
+
+
